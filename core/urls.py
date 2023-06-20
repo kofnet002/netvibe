780994aad92114b2a4c1ea05945cdba3b9a33cbd
Kofi Frimpong <noeljoel61@gmail.com>
2023-06-20 14:03:33 +0000

    +: register users

--- core/urls.py
+++ core/urls.py
@@ -22,8 +22,10 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chat.urls')),
+    path('', include('account.urls')),
     path("__reload__/", include("django_browser_reload.urls")),
 
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
