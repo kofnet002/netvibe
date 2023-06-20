d1074ee53df5ffd330f8bab61d41b2004016258d
Kofi Frimpong <noeljoel61@gmail.com>
2023-06-20 22:07:50 +0000

    +: login & logout

--- account/views.py
+++ account/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth import login, authenticate
-from account.forms import RegistrationForm
+from django.contrib.auth import login, authenticate, logout
+from account.forms import RegistrationForm, AccountAuthenticationForm
 
 
 # Create your views here.
@@ -23,7 +23,7 @@ def register_view(request, *args, **kwargs):
             account = authenticate(email, raw_password)
             login(request, account)
 
-            destination = kwargs.get('next')
+            destination = get_redirect_if_exists(request)
             if destination:
                 return redirect(destination)
             return redirect('home')
@@ -31,4 +31,42 @@ def register_view(request, *args, **kwargs):
             context['registration_form'] = form
 
     return render(request, 'account/register.html', context)            
-            
+
+
+def login_view(request, *args, **kwargs):
+
+    context = {}
+
+    user = request.user
+    if user.is_authenticated:
+        return redirect('home')
+
+    if request.method == 'POST':
+        form = AccountAuthenticationForm(request.POST)
+        if form.is_valid():
+            email = request.POST.get('email')
+            password = request.POST.get('password')
+            user = authenticate(email=email, password=password)
+
+            if user:
+                login(request, user)
+                destination = get_redirect_if_exists(request)
+                if destination:
+                    return redirect(destination)
+                return redirect('home')
+        else:
+            context['login_form'] = form     
+    return render(request, 'account/login.html', context)
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')
+
+
+def get_redirect_if_exists(request):
+    redirect = None
+    if request.GET:
+        if request.GET.get('next'):
+            redirect = str(request.GET.get('next'))
+    return redirect
